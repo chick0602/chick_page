2f14b8566dfa8fa58bbfce9eeae4187b64e4f7f9
chick0602 <chick0602@users.noreply.github.com>
2019-03-16 01:05:33 +0800

    修改js檔三張照片放大

--- lightboxjs/js.js
+++ lightboxjs/js.js
@@ -5,3 +5,17 @@ photo1.addEventListener("click",function(){
     openLightbox();
 
 });
+
+var photo2 = document.querySelector('.photo2');
+
+photo2.addEventListener("click",function(){
+    showImage(photo2);
+    openLightbox();
+})
+
+var photo3 = document.querySelector('.photo3');
+
+photo3.addEventListener("click",function(){
+    showImage(photo3);
+    openLightbox();
+})
