5a08b9e4616361d2b14e7c6b6792bfba7fafd2e6
chick0602 <chick0602@users.noreply.github.com>
2019-04-02 18:53:57 +0800

    Update js.js

--- lightboxjs/js.js
+++ lightboxjs/js.js
@@ -1,11 +1,15 @@
-var photo=[".photo1",".photo2",".photo3"];
-
-for(var i=0; i<3; i++){
-	photo=document.querySelector('photo[i]')
-}
-
-for(var i=0; i<3; i++){
-	photo.addEventListener("click",function(){
-		showImage(photo[i]);
-		openLightbox();
-})}
+var photo1 = document.querySelector('.photo1');
+var photo2 = document.querySelector('.photo2');
+var photo3 = document.querySelector('.photo3');
+photo1.addEventListener("click",function(){
+    showImage(photo1)
+    openLightbox();
+})
+photo2.addEventListener("click",function(){
+    showImage(photo2)
+    openLightbox();
+})
+photo3.addEventListener("click",function(){
+    showImage(photo3)
+    openLightbox();
+})
